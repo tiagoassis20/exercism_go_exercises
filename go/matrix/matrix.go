65ebc0e24037e91a86d36ca29796c4e154866069
Tiago Assis <t.assis@cinq.com.br>
2020-10-17 18:31:59 -0300

    resolved matrix problem

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -1,17 +1,66 @@
 package matrix
 
-type Matrix [][]int64
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-func New(s string) (*Matrix, error) {
+type Matrix [][]int
 
+func New(s string) (Matrix, error) {
+
+	rows := strings.Split(s, "\n")
+	if strings.Count(s, "\n") != len(rows)-1 {
+		return nil, fmt.Errorf("Row cannot be empty")
+	}
+	matrix := make(Matrix, len(rows))
+	nCols := 0
+	for i, row := range rows {
+		cols := strings.Split(strings.Trim(row, " "), " ")
+		if nCols != len(cols) {
+			if nCols == 0 {
+				nCols = len(cols)
+			} else {
+				return nil, fmt.Errorf("Uneven rows")
+			}
+		}
+		line := make([]int, nCols)
+		for j, col := range cols {
+			var err error
+			if line[j], err = strconv.Atoi(col); err != nil {
+				return nil, err
+			}
+		}
+		matrix[i] = line
+	}
+	return matrix, nil
 }
-func (m *Matrix) Rows() [][]int64 {
 
+func (m Matrix) Rows() [][]int {
+	matrix := make([][]int, len(m))
+	for i, row := range m {
+		matrix[i] = make([]int, len(row))
+		copy(matrix[i], m[i])
+	}
+	return matrix
 }
-func (m *Matrix) Cols() [][]int64 {
 
+func (m Matrix) Cols() [][]int {
+	matrix := make([][]int, len(m[0]))
+	for i := 0; i < len(m[0]); i++ {
+		matrix[i] = make([]int, len(m))
+		for j := 0; j < len(m); j++ {
+			matrix[i][j] = m[j][i]
+		}
+	}
+	return matrix
 }
 
-func (m *Matrix) Set(row, col int, val int64) [][]int64 {
-
+func (m Matrix) Set(row, col int, val int) bool {
+	if !(row >= 0 && row < len(m)) || !(col >= 0 && col < len(m[0])) {
+		return false
+	}
+	m[row][col] = val
+	return true
 }
