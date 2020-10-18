d349740dbf8be4b03e07fdfb769b8d0f3f436fda
Tiago Assis <t.assis@cinq.com.br>
2020-10-18 19:29:34 -0300

    resolved error hadling problem - recover method to panic and how to manage type assertions

---
+++ go/error-handling/error_handling.go
@@ -0,0 +1,33 @@
+package erratum
+
+func Use(o ResourceOpener, input string) (err error) {
+	resource, err := o()
+	
+	for err != nil {
+		if _, ok := err.(TransientError); !ok {
+			return
+		}
+		resource, err = o()
+	}
+
+	defer resource.Close()
+
+	defer func() {
+		if r := recover(); r != nil {
+			switch r := r.(type) {
+			case FrobError:
+				{
+					resource.Defrob(r.defrobTag)
+					err = r
+				}
+			case error:
+				err = r
+			}
+		}
+	}()
+
+	resource.Frob(input),
+
+	return
+
+}
