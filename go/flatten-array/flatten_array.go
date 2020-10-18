0e959a6d3911d1dd5e78ebb82c3a2f24f52a496a
Tiago Assis <t.assis@cinq.com.br>
2020-10-18 20:07:16 -0300

    Created first version of flatten array using recusivit and two for

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,1 +1,17 @@
 package flatten
+
+func Flatten(input interface{}) []interface{} {
+	if v, ok := input.([]interface{}); ok {
+		ret := make([]interface{}, 0, 20)
+		for _, e := range v {
+			for _, el := range Flatten(e) {
+				if el != nil {
+					ret = append(ret, el)
+				}
+			}
+		}
+		return ret
+	} else {
+		return []interface{}{input}
+	}
+}
